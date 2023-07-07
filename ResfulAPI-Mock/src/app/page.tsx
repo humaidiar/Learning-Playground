f07084f29b5fc277e416758608060b68c7f47366
humaidiar <humaidiar123@gmail.com>
2023-07-07 14:40:28 +1200

    working on POST

--- ResfulAPI-Mock/src/app/page.tsx
+++ ResfulAPI-Mock/src/app/page.tsx
@@ -14,17 +14,39 @@ export default async function Home() {
 
   const products: Product[] = await res.json()
 
+  const addProduct = async (e: FormData) => {
+    const product = e.get('product')?.toString()
+    const price = e.get('price')?.toString()
+
+    if (!product || !price) return
+
+    const newProduct: Product = {
+      product: product,
+      price: price,
+    }
+
+    await fetch('https://64a7731a096b3f0fcc814f7f.mockapi.io/products', {
+      method: 'POST',
+      body: JSON.stringify(newProduct),
+    })
+  }
+
   return (
     <main className="">
       <h1 className="text-3xl font-bold text-center">Product Warehouse</h1>
 
-      <form action="" className="flex flex-col gap-5 max-w-xl mx-auto p-5">
+      <form
+        action={addProduct}
+        className="flex flex-col gap-5 max-w-xl mx-auto p-5"
+      >
         <input
+          name="product"
           type="text"
           placeholder="Enter product name"
           className="border border-gray-300 p-2 rounded-md"
         />
         <input
+          name="price"
           type="text"
           placeholder="Enter price name"
           className="border border-gray-300 p-2 rounded-md"
